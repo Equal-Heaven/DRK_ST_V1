e71bc78ae283786137d4141b6461cf07c846ca3a
Equal-Heaven <145233988+Equal-Heaven@users.noreply.github.com>
2024-08-20 21:11:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,23 +6,23 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "darksten4@gmail.com";
+global.email = "lordbgpo@gmail.com";
 global.location = "America,Port-au-Prince.";
-global.mongodb = process.env.MONGODB_URI || "";
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "postgresql://sten_4ldx_user:9o1M5XMhT9FutGJ7gN1S90S39i9C0pQQ@dpg-cqr69ng8fa8c73foguig-a.oregon-postgres.render.com/sten_4ldx";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "America/Port-au-Prince";
-global.github = process.env.GITHUB || "https://github.com/Dark-S-TEN/DRK_ST_V1/tree/main";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vakp0UnICVfe3I2Fe72w";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vakp0UnICVfe3I2Fe72w";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/101bf22f101df83c4adbf.jpg";
-global.devs = "50931461936";
-global.sudo = process.env.SUDO || "50931461936";
-global.owner = process.env.OWNER_NUMBER || "50931461936";
+global.github = process.env.GITHUB || "https://github.com/Equal-Heaven/tree/main";
+global.gurl = process.env.GURL || "https://discord.com/invite/rPSKTXUc";
+global.website = process.env.GURL || "https://discord.com/invite/rPSKTXUc";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://cdn.discordapp.com/attachments/825089590943416418/1275541660226420826/7E47C068-518D-4021-9B71-DEA1558B5270.webp?ex=66c6443e&is=66c4f2be&hm=4d2cba928f0c6789a3380e519cc777bfd331022187f32c3e5a35eb10e59dc2bb&";
+global.devs = "242068312525";
+global.sudo = process.env.SUDO || "242068312525";
+global.owner = process.env.OWNER_NUMBER || "242068312525";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "true";
+global.wlcm = process.env.WELCOME || "false";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
@@ -48,11 +48,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "you're using first powerful bot to be released in DRK_ST_Tech| enjoy your time| this is DRK_ST_V1",
-  author: process.env.PACK_AUTHER || "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑",
+  caption: process.env.CAPTION || "JE SUIS UN ROBOT| soit heureux| this is 𝑩𝒓.𝒙",
+  author: process.env.PACK_AUTHER || "𝑩𝒓.𝒙",
   packname: process.env.PACK_NAME || "🤫",
-  botname: process.env.BOT_NAME || "DRK_ST_V1",
-  ownername: process.env.OWNER_NAME || "𝙎-𝙏𝞢𝞜",
+  botname: process.env.BOT_NAME || "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑",
+  ownername: process.env.OWNER_NAME || "𝑩𝒓.𝒙",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
